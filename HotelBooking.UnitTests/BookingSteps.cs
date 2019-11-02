0ea0a0e8aca7ee5d1582ef1c422363ece0915c98
patrickhansen12 <mussi_kat@hotmail.com>
2019-11-03 00:17:48 +0100

    Made the skeleton for the booking specflow
    //Bookinghotel.core
    
    Added my ideas of specflow tests on booking
    
    Add more tests by going into the feature file and click the generate steps after adding the desired senario¨

---
+++ HotelBooking.UnitTests/BookingSteps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace HotelBooking.UnitTests
+{
+    [Binding]
+    public class BookingSteps
+    {
+        int result;
+        //HotelBooking.Core.BookingManager bookingManager = new HotelBooking.Core.BookingManager();
+        [Given(@"I have entered an valid Start date")]
+        public void GivenIHaveEnteredAnValidStartDate(DateTime dateTime)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have entered an valid End date")]
+        public void GivenIHaveEnteredAnValidEndDate(DateTime dateTime)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press book")]
+        public void WhenIPressBook()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be room has been booked on the screen")]
+        public void ThenTheResultShouldBeRoomHasBeenBookedOnTheScreen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
