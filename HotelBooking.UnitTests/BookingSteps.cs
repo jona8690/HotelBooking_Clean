a69f22cbd0b0769232f2372824d876297d7347d6
Jonas Laursen <jona8690@edu.eal.dk>
2019-11-06 19:52:27 +0100

    Created booking steps

---
+++ HotelBooking.UnitTests/BookingSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace HotelBooking.UnitTests
+{
+    [Binding]
+    public class BookingSteps
+    {
+        [Given(@"That I check in (.*) days")]
+        public void GivenThatICheckInDays(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"That I check out in (.*)")]
+        public void GivenThatICheckOutIn(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"i try to book")]
+        public void WhenITryToBook()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"booking manager should return a room id")]
+        public void ThenBookingManagerShouldReturnARoomId()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"booking manager should throw Argument Exception")]
+        public void ThenBookingManagerShouldThrowArgumentException()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"booking manager should return (.*)")]
+        public void ThenBookingManagerShouldReturn(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
