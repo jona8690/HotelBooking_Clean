50ed3e6d66e8a095a86deb29050ad29a96dcf4a6
Jonas Laursen <jona8690@edu.eal.dk>
2019-11-06 20:01:28 +0100

    Implemented some booking steps

--- HotelBooking.UnitTests/BookingSteps.cs
+++ HotelBooking.UnitTests/BookingSteps.cs
@@ -1,33 +1,51 @@
-﻿using System;
+﻿using HotelBooking.Core;
+using HotelBooking.UnitTests.Fakes;
+using System;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace HotelBooking.UnitTests
 {
     [Binding]
     public class BookingSteps
     {
-        [Given(@"That I check in (.*) days")]
+		private IBookingManager bookingManager;
+
+		private DateTime BookingStart;
+		private DateTime BookingEnd;
+		private int RoomId;
+
+		public BookingSteps()
+		{
+			DateTime start = DateTime.Today.AddDays(10);
+			DateTime end = DateTime.Today.AddDays(20);
+			IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
+			IRepository<Room> roomRepository = new FakeRoomRepository();
+			bookingManager = new BookingManager(bookingRepository, roomRepository);
+		}
+
+		[Given(@"That I check in (.*) days")]
         public void GivenThatICheckInDays(int p0)
         {
-            ScenarioContext.Current.Pending();
+			this.BookingStart = DateTime.Now.AddDays(p0);
         }
         
         [Given(@"That I check out in (.*)")]
         public void GivenThatICheckOutIn(int p0)
         {
-            ScenarioContext.Current.Pending();
+			this.BookingEnd = DateTime.Now.AddDays(p0);
         }
         
         [When(@"i try to book")]
         public void WhenITryToBook()
         {
-            ScenarioContext.Current.Pending();
-        }
+			this.RoomId = bookingManager.FindAvailableRoom(BookingStart, BookingEnd);
+		}
         
         [Then(@"booking manager should return a room id")]
         public void ThenBookingManagerShouldReturnARoomId()
         {
-            ScenarioContext.Current.Pending();
+			Assert.True(this.RoomId > 0); // A room id has been assigned
         }
         
         [Then(@"booking manager should throw Argument Exception")]
@@ -39,7 +57,7 @@ namespace HotelBooking.UnitTests
         [Then(@"booking manager should return (.*)")]
         public void ThenBookingManagerShouldReturn(int p0)
         {
-            ScenarioContext.Current.Pending();
+			Assert.Equal(p0, this.RoomId);
         }
     }
 }
